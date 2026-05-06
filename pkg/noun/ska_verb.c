2babe8fb8f3c282a24527f4a7dc1fed0d0e34849
dozreg-toplud <afonin.k.u@gmail.com>
2026-05-06 11:03:02 +0200

    update hoon part

--- pkg/noun/ska_verb.c
+++ pkg/noun/ska_verb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6054544f0cf2409ab788c1d41a39c715d3add8ee2d9d155adfb45724d70a2737
-size 11096116
+oid sha256:2a149d27899d104f6ac93ab7b7ce1ab4700d44004c355309cfd944a76cb475df
+size 10659084
